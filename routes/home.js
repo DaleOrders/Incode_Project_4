ea95ba2c6e55d792f9f3cf30ec7df80afa3e63c5
Dale Orders <daleorders7@gmail.com>
2021-06-23 17:50:50 +1000

    added headings and link to individual pages

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const { redirectToLogin } = require('../middleware')
 
 router.get('/', redirectToLogin, (req,res) => {
-    db.any('SELECT surname, first_name, day, TO_CHAR(start_at,\'fmHH12:MI AM\') as start_at, TO_CHAR(end_at,\'fmHH12:MI AM\') as end_at FROM users INNER JOIN schedules ON users.id=schedules.user_id')
+    db.any('SELECT user_id, surname, first_name, day, TO_CHAR(start_at,\'fmHH12:MI AM\') as start_at, TO_CHAR(end_at,\'fmHH12:MI AM\') as end_at FROM users INNER JOIN schedules ON users.id=schedules.user_id')
         .then((result => {
             res.render('pages/homepage',{
                 result: result,
--- routes/individualEmployee.js
+++ routes/individualEmployee.js
@@ -3,15 +3,17 @@ const express = require('express')
 const db = require('../database')
 const router = express.Router()
 
-router.get('/', (req,res)=>{
-    res.render('pages/individualEmployee',{
-        title:result
+router.get('/:id(\\d+)', (req,res)=>{
+    db.any('SELECT first_name, surname, email, day, TO_CHAR(start_at,\'fmHH12:MI AM\') as start_at, TO_CHAR(end_at,\'fmHH12:MI AM\') as end_at FROM users INNER JOIN schedules ON users.id=schedules.user_id WHERE users.id=$1;',[req.params.id])
+    .then((result)=>{
+        console.log(result)
+        res.render('pages/employeePage',{
+            result: result,
+            documentTitle: "Employee Page"
+        })
     })
-})
-
-router.post('/', (req,res)=>{
-    res.render('pages/individualEmployee',{
-        result:result
+    .catch((err)=>{
+        console.log(err.message)
     })
 })
 
