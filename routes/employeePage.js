a230e8929917bcf1da8cabaf39e235a2e7e42a2a
Dale Orders <daleorders7@gmail.com>
2021-06-24 14:12:59 +1000

    added delete function

--- routes/employeePage.js
+++ routes/employeePage.js
@@ -6,9 +6,7 @@ const router = express.Router()
 const { redirectToLogin } = require('../middleware')
 
 router.get('/', redirectToLogin, (req,res)=>{
-    db.any(
-        'SELECT first_name, surname, email, day, TO_CHAR(start_at,\'fmHH12:MI AM\') as start_at, TO_CHAR(end_at,\'fmHH12:MI AM\') as end_at FROM users INNER JOIN schedules ON users.id=schedules.user_id WHERE users.id=$1;',
-        [req.session.userId])
+    db.any('SELECT first_name, surname, email, day, TO_CHAR(start_at,\'fmHH12:MI AM\') as start_at, TO_CHAR(end_at,\'fmHH12:MI AM\') as end_at FROM users INNER JOIN schedules ON users.id=schedules.user_id WHERE users.id=$1;',[req.session.userId])
     .then((result)=>{
         console.log(result)
         res.render('pages/employeePage',{
--- routes/scheduleManagement.js
+++ routes/scheduleManagement.js
@@ -6,7 +6,7 @@ const { redirectToLogin } = require('../middleware')
 //form to add new schedule
 
 router.get('/', redirectToLogin, (req,res) => {
-    db.any(`SELECT first_name, surname, day, TO_CHAR(start_at,\'fmHH12:MI AM\') as start_at, TO_CHAR(end_at,\'fmHH12:MI AM\') as end_at FROM schedules INNER JOIN users ON schedules.user_id=users.id WHERE user_id = $1;`, [req.session.userId])
+    db.any(`SELECT user_id, first_name, surname, day, TO_CHAR(start_at,\'fmHH12:MI AM\') as start_at, TO_CHAR(end_at,\'fmHH12:MI AM\') as end_at FROM schedules INNER JOIN users ON schedules.user_id=users.id WHERE user_id = $1;`, [req.session.userId])
             .then((result) => {
                 res.render('pages/scheduleManagement', {
                     result: result,
@@ -17,6 +17,20 @@ router.get('/', redirectToLogin, (req,res) => {
             console.log(err.message)
             res.send(err)
         })
+})
+
+router.get('/:id(\\d+)', (req,res)=>{
+    db.any('DELETE FROM schedules WHERE schedules.user_id=$1',[req.params.id])
+    .then((result)=>{
+        res.render('pages/scheduleManagement', {
+            result:result,
+            documentTitle: "Schedules"
+        })
+    })
+    .catch((err)=>{
+        console.log(err)
+        res.send(err)
+    })
 })
 
 router.post('/', redirectToLogin, (req,res)=>{
